d414ce534e755a2e9912b49988b71996be879986
Steffen Gudis <steffen.gudis@nexum.ch>
2016-11-11 14:36:02 +0100

    Auto bootstrapping.

--- Module.php
+++ Module.php
@@ -80,54 +80,6 @@ class Module extends \yii\base\Module implements BootstrapInterface {
     Yii::$app->params['defaultLanguage'] = 'de-CH';
   }
 
-  /**
-   * [bootstrap description]
-   * @param  [type] $app [description]
-   * @return [type]      [description]
-   */
-  public function bootstrap($app) {
-
-    if ($app instanceof \yii\web\Application) {
-      /**
-       * Adding i18n component here.
-       */
-      Yii::$app->setComponents([
-        'i18n' => [
-          'class' => 'yii\i18n\I18N',
-          'translations' => [
-            'app*' => [
-              'class' => 'yii\i18n\PhpMessageSource',
-              'basePath' => '@app/messages',
-              'sourceLanguage' => 'en-US',
-              'fileMap' => [
-                'app' => 'app.php',
-                'app/error' => 'error.php',
-              ],
-              'on missingTranslation' => [CrelishI18nEventHandler::class, 'handleMissingTranslation']
-            ],
-          ],
-        ]
-      ]);
-
-      $app->getUrlManager()->addRules([
-        ['class' => 'yii\web\UrlRule', 'pattern' => 'crelish/<controller:[\w\-]+>/<action:[\w\-]+>', 'route' => 'crelish/<controller>/<action>'],
-        ['class' => 'yii\web\UrlRule', 'pattern' => 'crelish/<id:\w+>', 'route' => 'crelish/default/view'],
-        ['class' => 'yii\web\UrlRule', 'pattern' =>  'crelish', 'route' => 'crelish/default/index'],
-        ['class' => 'yii\web\UrlRule', 'pattern' => '<controller:[\w\-]+>/<action:[\w\-]+>', 'route' => '/<controller>/<action>'],
-        ['class' => 'giantbits\crelish\components\CrelishBaseUrlRule']
-        //['class' => 'yii\web\UrlRule', 'pattern' => '<lang:[\w\-]+]>/<controller:[\w\-]+>/<action:[\w\-]+>', 'route' => '/<controller>/<action>']
-      ], TRUE);
-    } elseif ($app instanceof \yii\console\Application) {
-      $app->controllerMap[$this->id] = [
-        'class' => 'yii\gii\console\GenerateController',
-        'generators' => array_merge($this->coreGenerators(), $this->generators),
-        'module' => $this,
-      ];
-    }
-
-    Yii::$app->setModules(['redactor' => 'yii\redactor\RedactorModule']);
-  }
-
   public function beforeAction($action)
   {
     if (!parent::beforeAction($action)) {
--- controllers/SettingsController.php
+++ controllers/SettingsController.php
@@ -8,71 +8,74 @@ use giantbits\crelish\models\LoginForm;
 use yii\filters\AccessControl;
 
 
-
 class SettingsController extends CrelishBaseController
 {
-    public $layout = 'crelish.twig';
+  public $layout = 'crelish.twig';
 
-    public function behaviors()
-    {
-        return [
-            'access' => [
-                'class' => AccessControl::className(),
-                'rules' => [
-                    [
-                        'allow' => true,
-                        'actions' => ['login'],
-                        'roles' => ['?'],
-                    ],
-                    [
-                        'allow' => true,
-                        'actions' => [],
-                        'roles' => ['@'],
-                    ],
-                ],
-            ],
-        ];
+  public function behaviors()
+  {
+    return [
+      'access' => [
+        'class' => AccessControl::className(),
+        'rules' => [
+          [
+            'allow' => true,
+            'actions' => ['login'],
+            'roles' => ['?'],
+          ],
+          [
+            'allow' => true,
+            'actions' => [],
+            'roles' => ['@'],
+          ],
+        ],
+      ],
+    ];
+  }
+
+
+  public function init()
+  {
+    parent::init();
+  }
+
+  public function actionIndex()
+  {
+    return $this->render('index.twig');
+  }
+
+  public function actionClearcache()
+  {
+    \Yii::$app->cache->flush();
+    return $this->redirect('/crelish/settings/index', 302);
+  }
+
+  public function actionClearwebassets()
+  {
+    $filePath = \Yii::getAlias('@app/web/assets');
+    $files = scandir($filePath);
+    foreach ($files as $f) {
+      if (is_dir($filePath . DIRECTORY_SEPARATOR . $f) && substr($f, 0, 1) != '.') {
+        $this->deleteRecursive($filePath . DIRECTORY_SEPARATOR . $f);
+      }
     }
+    return $this->redirect('/crelish/settings/index', 302);
+  }
 
-
-    public function init()
-    {
-      parent::init();
-    }
-
-    public function actionIndex() {
-      return $this->render('index.twig');
-    }
-
-    public function actionClearcache() {
-      \Yii::$app->cache->flush();
-      return $this->redirect('/crelish/settings/index',302);
-    }
-
-    public function actionClearwebassets() {
-      $filePath = \Yii::getAlias('@app/web/assets');
-      $files = scandir($filePath);
-      foreach ($files as $f) {
-        if (is_dir($filePath . DIRECTORY_SEPARATOR . $f) && substr($f,0,1)!='.') {
-            $this->deleteRecursive($filePath . DIRECTORY_SEPARATOR . $f);
+  private function deleteRecursive($folder)
+  {
+    $files = scandir($folder);
+    foreach ($files as $file) {
+      if (!in_array($file, array('.', '..'))) {
+        $path = $folder . DIRECTORY_SEPARATOR . $file;
+        if (is_dir($path)) {
+          $this->deleteRecursive($path);
+        } else {
+          unlink($path);
         }
       }
-      return $this->redirect('/crelish/settings/index',302);
     }
-
-    private function deleteRecursive($folder) {
-        $files = scandir($folder);
-        foreach ($files as $file) {
-            if (!in_array($file, array('.','..'))) {
-                $path = $folder . DIRECTORY_SEPARATOR . $file;
-                if (is_dir($path)) {
-                    $this->deleteRecursive($path);
-                } else {
-                    unlink($path);
-                }
-            }
-        }
-        rmdir($folder);
-    }
+    rmdir($folder);
+  }
 
 }
