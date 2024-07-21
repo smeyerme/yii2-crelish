4a9d635ed3b26dc36514c3bb622acdfa504697b6
Steffen Meyer <myr.steffen@gmail.com>
2024-07-21 14:24:26 +0200

    feat: Update Yii2 version and fixes for matrix

--- widgets/ElementNav.php
+++ widgets/ElementNav.php
@@ -13,13 +13,16 @@ class ElementNav extends Widget
   private $action;
   private $ctype;
   private $selector = 'ctype';
+	
+	private $target = '#contentSelector';
 
   public function __construct($config = [])
   {
-    if (count($config) > 0) {
+		if (count($config) > 0) {
       $this->action = (empty($config['action'])) ? \Yii::$app->controller->action->id : $config['action'];
-      $this->selector = (empty($config['selector'])) ? 'ctype' : $config['selector'];
-      $this->ctype = (empty($config[$this->selector])) ? '' : $config[$this->selector];
+      $this->selector = (empty($config['selector'])) ? $this->selector : $config['selector'];
+      $this->ctype = (empty($config['ctype'])) ? $this->ctype : $config['ctype'];
+      $this->target = (empty($config['target'])) ? $this->target : $config['target'];
     } else {
       $this->action = \Yii::$app->controller->action->id;
       $this->ctype = !empty(\Yii::$app->getRequest()
@@ -45,8 +48,8 @@ class ElementNav extends Widget
       'sort' => ['by' => ['category', 'label', 'asc']],
       'limit' => 99
     ]);
-
-    $params[0] = 'content/' . $this->action;
+		
+    $params[0] = \Yii::$app->controller->id . '/' . $this->action;
 
     foreach (\Yii::$app->getRequest()->getQueryParams() as $param => $value) {
       $params[$param] = $value;
@@ -61,7 +64,7 @@ class ElementNav extends Widget
       $css = ($this->ctype == $element['key']) ? 'gc-active-filter' : '';
       $params[$this->selector] = $element['key'];
       $targetUrl = Url::to($params);
-      $nav .= Html::tag('li', Html::a(Html::tag('span', $element['label']), $targetUrl, ['data-pjax'=>1]), ['class' => $css]);
+      $nav .= Html::tag('li', Html::a(Html::tag('span', $element['label']), $targetUrl, ['data-pjax'=>1, 'data-target'=>$this->target]), ['class' => $css]);
     }
 
     return $nav;
