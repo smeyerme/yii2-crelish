5c0f0df1ce8900656cdcbd08b76669336fc6c41b
Steffen Meyer <myr.steffen@gmail.com>
2024-12-12 22:21:56 +0100

    feat: set default sorting for export

--- Bootstrap.php
+++ Bootstrap.php
@@ -226,6 +226,6 @@ class Bootstrap implements BootstrapInterface
       ]
     ]);
 
-    \Yii::$app->params['crelish']['version'] = 'V0.4.45';
+    \Yii::$app->params['crelish']['version'] = 'V0.4.46';
   }
 }
