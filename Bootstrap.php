24f978566ca3755086a062eb9686cda5dfbc97ee
Steffen Meyer <myr.steffen@gmail.com>
2025-03-14 23:59:43 +0100

    feat: Better translation interface

--- Bootstrap.php
+++ Bootstrap.php
@@ -239,6 +239,6 @@ class Bootstrap implements BootstrapInterface
       ]
     ]);
 
-    \Yii::$app->params['crelish']['version'] = 'V0.7.6';
+    \Yii::$app->params['crelish']['version'] = 'V0.7.7';
   }
 }
